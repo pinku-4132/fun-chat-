9ad1bb575e8fa99669f6f03e0a58504d916a43ab
unknown <pinku4132@gmail.com>
2017-06-13 16:26:05 +0530

    broadcasing typing.. in chat windows

--- public/chat.js
+++ public/chat.js
@@ -4,7 +4,15 @@ var socket=io.connect('http://localhost:2000');
 var message=document.getElementById('message');
     handler=document.getElementById('handler'),
     btnsend=document.getElementById('send'),
-    output=document.getElementById('output');
+    output=document.getElementById('output'),
+    feedback=document.getElementById('feedback');
+
+//sending typing... data message
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handler.value);
+})
+
+
 //sending data to the server side 
 btnsend.addEventListener('click',function(){
     socket.emit('chat',{
@@ -14,6 +22,11 @@ btnsend.addEventListener('click',function(){
 });
 
 socket.on('chat',function(data){
+    feedback.innerHTML='';
 output.innerHTML+= '<p><strong>'+data.handler+':</strong>'+data.message+'</p>';
 })
+
+socket.on('typing',function(data){
+    feedback.innerHTML='<p><em>'+ data + ' typing... a  message </em></p>';
+});
 
